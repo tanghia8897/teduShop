b764c299a4222093d549cabf79fc21bbd6b07905
tanghia8897 <tanghia080897@gmail.com>
2018-11-06 14:50:59 +0700

    add domain entity for project

--- TeduShop.Model/Properties/AssemblyInfo.cs
+++ TeduShop.Model/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TeduShop.Model")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fa2a5fd7-fb79-459d-8cc2-13eb3f073b3c")]
+[assembly: Guid("1a60e027-7b3b-4881-9ec3-2666b297cb43")]
 
 // Version information for an assembly consists of the following four values:
 //
